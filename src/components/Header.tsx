feba245a7ab86554fd4ddaeb379b0a9ab6e74793
Andrew Rowley <andrew@internetdrew.com>
2023-12-02 14:41:50 -0500

    add some responsive styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
-    <header className='max-w-2xl mx-auto text-center mt-12'>
-      <h1 className='text-4xl font-bold'>Welcome to NYC Crash Radar</h1>
-      <h2 className='text-2xl'>
+    <header className='max-w-2xl mx-auto text-center mt-12 px-4 sm:px-0'>
+      <h1 className='text-3xl sm:text-4xl font-bold'>NYC Crash Radar</h1>
+      <h2 className='text-xl sm:text-2xl'>
         See reported vehicle crashes around New York City.
       </h2>
     </header>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,16 @@
+import { HiMenu } from 'react-icons/hi';
+
 const Navbar = () => {
   return (
-    <nav className='py-4'>
+    <nav className='py-4 px-4'>
       <div className='flex items-center justify-between max-w-7xl mx-auto'>
         <div className='text-2xl font-semibold'>NYC Crash Radar</div>
-        <ul>
-          <li>About</li>
-        </ul>
+        <div>
+          <ul>
+            <li>About</li>
+          </ul>
+          <HiMenu className='sm:hidden w-7 h-7' />
+        </div>
       </div>
     </nav>
   );
