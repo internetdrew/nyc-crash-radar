0dd621de6c23d2a0c96f459b49eb6b77f7963b29
Andrew Rowley <andrew@internetdrew.com>
2023-12-02 14:26:50 -0500

    remove header bg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
 const Header = () => {
   return (
-    <header className='bg-red-200 max-w-2xl mx-auto text-center mt-10'>
-      <h1 className='text-3xl font-semibold'>Welcome to NYC Crash Radar</h1>
-      <h2>See reported vehicle crashes around New York City</h2>
+    <header className='max-w-2xl mx-auto text-center mt-12'>
+      <h1 className='text-4xl font-bold'>Welcome to NYC Crash Radar</h1>
+      <h2 className='text-2xl'>
+        See reported vehicle crashes around New York City.
+      </h2>
     </header>
   );
 };
