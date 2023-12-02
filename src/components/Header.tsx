a49d3a336f28cbf2f3a6a9c54f0f3959e10c0646
Andrew Rowley <andrew@internetdrew.com>
2023-12-02 14:24:04 -0500

    improve vertical padding in navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,10 @@
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className='bg-red-200 max-w-2xl mx-auto text-center mt-10'>
+      <h1 className='text-3xl font-semibold'>Welcome to NYC Crash Radar</h1>
+      <h2>See reported vehicle crashes around New York City</h2>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className='py-2'>
+    <nav className='py-4'>
       <div className='flex items-center justify-between max-w-7xl mx-auto'>
         <div className='text-2xl font-semibold'>NYC Crash Radar</div>
         <ul>
