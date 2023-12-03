188bbfb85c2df6b80b35266e7cf93fc104208cfb
Andrew Rowley <andrew@internetdrew.com>
2023-12-03 13:27:53 -0500

    update nav styles for responsive

--- src/components/CrashFeed.tsx
+++ src/components/CrashFeed.tsx
@@ -1,5 +1,7 @@
 const CrashFeed = () => {
-  return <div>CrashFeed</div>;
+  return (
+    <main className='bg-red-200 max-w-4xl mx-auto mt-20 px-4'>CrashFeed</main>
+  );
 };
 
 export default CrashFeed;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,27 @@
-import { HiMenu } from 'react-icons/hi';
+import { useState } from 'react';
+import { HiMenu, HiX } from 'react-icons/hi';
 
 const Navbar = () => {
+  const [mobileNavIsOpen, setMobileNavIsOpen] = useState(false);
+
   return (
-    <nav className='py-4 px-4'>
+    <nav className='p-4'>
       <div className='flex items-center justify-between max-w-7xl mx-auto'>
         <div className='text-2xl font-semibold'>NYC Crash Radar</div>
         <div>
           <ul>
             <li>About</li>
           </ul>
-          <HiMenu className='sm:hidden w-7 h-7' />
+          <button
+            className='sm:hidden'
+            onClick={() => setMobileNavIsOpen(!mobileNavIsOpen)}
+          >
+            {mobileNavIsOpen ? (
+              <HiX className='w-7 h-7' />
+            ) : (
+              <HiMenu className='w-7 h-7' />
+            )}
+          </button>
         </div>
       </div>
     </nav>
