0ddff381a4fbe07c1a4d9c166d222f3066acf282
Andrew Rowley <andrew@internetdrew.com>
2023-12-04 16:25:42 -0500

    move vehicle icon map to its own file

--- src/components/CrashFeed.tsx
+++ src/components/CrashFeed.tsx
@@ -1,58 +1,9 @@
+import React from 'react';
 import { fetchCrashes } from '@api/fetchCrashes';
 import { formatLocationString } from '@helpers/formatLocationString';
 import { formatDate } from '@/helpers/formatDate';
 import { formatVehicle } from '@/helpers/formatVehicle';
-import { TbCarSuv } from 'react-icons/tb';
-import { FaCar } from 'react-icons/fa';
-import { FaTruckPickup } from 'react-icons/fa';
-import { GiTruck } from 'react-icons/gi';
-import { FaTruck } from 'react-icons/fa';
-import { GiFuelTank } from 'react-icons/gi';
-import { MdOutlinePedalBike } from 'react-icons/md';
-import { FaBus } from 'react-icons/fa6';
-import { FaTaxi } from 'react-icons/fa';
-import { PiVanBold } from 'react-icons/pi';
-import { GiScooter } from 'react-icons/gi';
-import { MdOutlineElectricBike } from 'react-icons/md';
-import { MdOutlineElectricScooter } from 'react-icons/md';
-import { GiAmbulance } from 'react-icons/gi';
-import { GiMineTruck } from 'react-icons/gi';
-import { HiTruck } from 'react-icons/hi2';
-import { PiMopedFront } from 'react-icons/pi';
-import { FaMotorcycle } from 'react-icons/fa';
-import { MdCarCrash } from 'react-icons/md';
-import { GiTowTruck } from 'react-icons/gi';
-import { TbFiretruck } from 'react-icons/tb';
-import { MdForklift } from 'react-icons/md';
-import React from 'react';
-
-const iconClasses = 'mx-auto w-8 h-8';
-
-const iconMapping: Record<string, JSX.Element> = {
-  'Station Wagon/Sport Utility Vehicle': <TbCarSuv className={iconClasses} />,
-  Sedan: <FaCar className={iconClasses} />,
-  'Pick-up Truck': <FaTruckPickup className={iconClasses} />,
-  'Tractor Truck Diesel': <GiTruck className={iconClasses} />,
-  Dump: <GiMineTruck className={iconClasses} />,
-  Tanker: <GiFuelTank className={iconClasses} />,
-  Bike: <MdOutlinePedalBike className={iconClasses} />,
-  Bus: <FaBus className={iconClasses} />,
-  Taxi: <FaTaxi className={iconClasses} />,
-  Van: <PiVanBold className={iconClasses} />,
-  Motorscooter: <GiScooter className={iconClasses} />,
-  'E-Bike': <MdOutlineElectricBike className={iconClasses} />,
-  'E-Scooter': <MdOutlineElectricScooter className={iconClasses} />,
-  Ambulance: <GiAmbulance className={iconClasses} />,
-  AMBULANCE: <GiAmbulance className={iconClasses} />,
-  'Box Truck': <FaTruck className={iconClasses} />,
-  Truck: <FaTruck className={iconClasses} />,
-  'Garbage or Refuse': <HiTruck className={iconClasses} />,
-  Moped: <PiMopedFront className={iconClasses} />,
-  Motorcycle: <FaMotorcycle className={iconClasses} />,
-  'Tow Truck / Wrecker': <GiTowTruck className={iconClasses} />,
-  'Fdny fire': <TbFiretruck className={iconClasses} />,
-  FORKLIFT: <MdForklift className={iconClasses} />,
-};
+import { iconMapping, DefaultVehicleIcon } from '@/utils/vehicleIconMap';
 
 const CrashFeed = () => {
   const { isPending, error, data, fetchNextPage, isFetching } = fetchCrashes();
@@ -92,7 +43,9 @@ const CrashFeed = () => {
                   <section className='mt-6'>
                     {crash?.vehicle_type_code1 && (
                       <p className='mb-2 text-gray-800'>
-                        {crash?.vehicle_type_code2 ? 'Vehicles' : 'Vehicle'}{' '}
+                        {crash?.vehicle_type_code2
+                          ? 'Vehicles Involved'
+                          : 'Vehicle'}{' '}
                         Involved
                       </p>
                     )}
@@ -110,10 +63,10 @@ const CrashFeed = () => {
                               key={`vehicle ${index}`}
                               className='ring-1 ring-slate-950 w-full text-center p-2 mx-auto rounded-lg font-semibold mt-auto text-xs sm:text-sm'
                             >
-                              <p>Vehicle {index + 1}</p>
-                              {iconMapping[vehicle] ?? (
-                                <MdCarCrash className={iconClasses} />
+                              {crash?.vehicle_type_code2 && (
+                                <p>Vehicle {index + 1}</p>
                               )}
+                              {iconMapping[vehicle] ?? <DefaultVehicleIcon />}
                               {formatVehicle(vehicle)}
                             </article>
                           );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,9 @@
-import { useState } from 'react';
-import { HiMenu, HiX } from 'react-icons/hi';
-
 const Navbar = () => {
-  const [mobileNavIsOpen, setMobileNavIsOpen] = useState(false);
-
   return (
     <nav className='px-4 py-3 backdrop-blur-sm sticky top-0 sm:backdrop-blur-none'>
       <div className='flex items-center justify-between max-w-7xl mx-auto'>
         <div className='text-2xl font-bold bg-black text-white px-4 py-2'>
           NYC Crash Radar
-        </div>
-        <div>
-          <ul>
-            <li>About</li>
-          </ul>
-          <button
-            className='sm:hidden'
-            onClick={() => setMobileNavIsOpen(!mobileNavIsOpen)}
-          >
-            {mobileNavIsOpen ? (
-              <HiX className='w-7 h-7' />
-            ) : (
-              <HiMenu className='w-7 h-7' />
-            )}
-          </button>
         </div>
       </div>
     </nav>
