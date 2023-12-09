a38745b5739ec1ed679f63ab4ffe6241e892dff9
Andrew Rowley <andrew@internetdrew.com>
2023-12-09 13:30:49 -0500

    improve z indexing for mobile scroll

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className='px-4 pt-3 backdrop-blur-sm sticky top-0 sm:backdrop-blur-none'>
+    <nav className='px-4 pt-3 backdrop-blur-sm sticky top-0 z-10 sm:backdrop-blur-none'>
       <div className='flex items-center justify-between max-w-7xl mx-auto'>
         <div className='text-xl font-bold bg-black text-stone-200 px-4 py-2'>
           NYC Crash Radar
