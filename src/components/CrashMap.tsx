10b6ee0ae5950e2eceeea8dcbd220a1aeb93710d
Andrew Rowley <andrew@internetdrew.com>
2023-12-09 02:54:49 -0500

    add custom map icon

--- src/components/CrashMap.tsx
+++ src/components/CrashMap.tsx
@@ -1,5 +1,7 @@
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { Icon } from 'leaflet';
+import { FaMapMarkerAlt } from 'react-icons/fa';
 
 interface CrashMapProps {
   lat: number;
@@ -7,17 +9,23 @@ interface CrashMapProps {
 }
 
 const CrashMap = (coords: CrashMapProps) => {
+  const mapIcon = new Icon({
+    iconUrl:
+      'https://cdn4.iconfinder.com/data/icons/small-n-flat/24/map-marker-512.png',
+    iconSize: [38, 38],
+  });
   return (
     <MapContainer
       center={[coords.lat, coords.lng]}
-      zoom={20}
+      zoom={15}
       scrollWheelZoom={false}
+      dragging={false}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
-      <Marker position={[coords.lat, coords.lng]} />
+      <Marker position={[coords.lat, coords.lng]} icon={mapIcon} />
     </MapContainer>
   );
 };
