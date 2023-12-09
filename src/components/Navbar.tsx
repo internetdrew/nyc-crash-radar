f58d372c3b554a76e5b4b43a2b9a6162732bd3f2
Andrew Rowley <andrew@internetdrew.com>
2023-12-09 12:45:07 -0500

    add new favicon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (
     <nav className='px-4 pt-3 backdrop-blur-sm sticky top-0 sm:backdrop-blur-none'>
       <div className='flex items-center justify-between max-w-7xl mx-auto'>
-        <div className='text-xl font-bold bg-black text-white px-4 py-2'>
+        <div className='text-xl font-bold bg-black text-stone-200 px-4 py-2'>
           NYC Crash Radar
         </div>
       </div>
