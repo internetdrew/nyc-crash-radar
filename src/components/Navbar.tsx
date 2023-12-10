04edd588a24c929428539a0ab49f51484383b0e8
Andrew Rowley <andrew@internetdrew.com>
2023-12-10 12:21:39 -0500

    add link to about route

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,20 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
   return (
     <nav className='px-4 py-3 backdrop-blur-sm sticky top-0 z-10 sm:backdrop-blur-none'>
       <div className='flex items-center justify-between max-w-7xl mx-auto'>
-        <div className='text-xl font-bold bg-black text-stone-200 px-4 py-2'>
+        <Link
+          to='/'
+          className='text-xl font-bold bg-black text-stone-200 px-4 py-2'
+        >
           NYC Crash Radar
-        </div>
+        </Link>
+        <ul>
+          <li>
+            <Link to={'/about'}>About</Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
